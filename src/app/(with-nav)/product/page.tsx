b247cc33c23e166c0cbb492a43fdab562e9d1c76
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-27 09:52:59 +0200

    Prettier format

--- src/app/(with-nav)/product/page.tsx
+++ src/app/(with-nav)/product/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: "Marketplace - post ad",
 };
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 async function getCategories(): Promise<ProductCategoryDTO[] | undefined> {
   const categories: ProductCategoryDTO[] = await getAllProductCategories()
